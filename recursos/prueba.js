1efa68e31ac957a2e93f174d5463148efb6580ae
ermarc <91552736+ermarc@users.noreply.github.com>
2022-09-27 22:29:37 +0000

    Add files via upload

--- recursos/prueba.js
+++ recursos/prueba.js
@@ -19,6 +19,26 @@ function entrarPagina() {
     setTimeout( function() {
         var borrarDivBlanco = document.getElementById("primero");
         borrarDivBlanco.remove()
-        crearAudio();
     }, 1200);
+}
+
+function jumpScare() {
+    var crearAudio = document.createElement("audio");
+    crearAudio.autoplay = "autoplay";
+    crearAudio.loop = "loop";
+    crearAudio.id = "nuevoAudio"
+    var crearSource = document.createElement("source");
+    crearSource.src = "recursos/exorcista.mp3";
+    crearSource.type = "audio/mpeg";
+
+    document.body.appendChild(crearAudio);
+    document.getElementById("nuevoAudio").appendChild(crearSource);
+
+    var entrarPaginaLink = document.createElement("link");
+    entrarPaginaLink.href = "recursos/css/newer.css";
+    entrarPaginaLink.rel = "stylesheet";
+    entrarPaginaLink.type = "text/css";
+    entrarPaginaLink.id = "tula";
+    document.head.appendChild(entrarPaginaLink);
+
 }
