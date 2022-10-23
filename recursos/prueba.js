cc07c665944bc0d45f3ce176daa0f61a8ff3aa76
ermarc <91552736+ermarc@users.noreply.github.com>
2022-10-23 17:53:02 +0200

    Updated select

--- recursos/prueba.js
+++ recursos/prueba.js
@@ -1,27 +1,14 @@
-function crearAudio() {
-    var crearAudio = document.createElement("audio");
-    crearAudio.autoplay = "autoplay";
-    crearAudio.loop = "loop";
-    crearAudio.id = "nuevoAudio"
-    var crearSource = document.createElement("source");
-    crearSource.src = "recursos/musica.mp3";
-    crearSource.type = "audio/mpeg";
-
-    document.body.appendChild(crearAudio);
-    document.getElementById("nuevoAudio").appendChild(crearSource);
-}
-
 function entrarPagina() {
-    var entrarPaginaLink = document.createElement("link");
-    entrarPaginaLink.href = "recursos/css/entrarPagina.css";
-    entrarPaginaLink.rel = "stylesheet";
-    entrarPaginaLink.type = "text/css";
-    document.head.appendChild(entrarPaginaLink);
+    document.getElementById("primero").onclick = "";
+    document.getElementById("primero").style.animation = "desvanecer 0.5s forwards";
+    document.getElementById("titulo").style.animation = "brilloAmarillo 1.8s infinite alternate ease-in-out, simpleSkyDrop 0.38s forwards linear, continuousBounce 0.18s forwards infinite ease-out alternate";
+    document.getElementById("titulo").style.animationDelay = "0s, 0s, 0.38s";
+    document.getElementById("versionText").style.animation = "desvanecer 0.5s reverse forwards 1.2s";
+    document.getElementById("musicaFondo").play();
     setTimeout( function() {
         var borrarDivBlanco = document.getElementById("primero");
         borrarDivBlanco.remove();
     }, 550);
-    crearAudio();
     setTimeout( function() {
         crearArchivos();
     })
@@ -39,54 +26,24 @@ function crearArchivos() {
     crearPenjatPrimitive.textContent = "primitive";
     crearPenjatPrimitive.id = "singleButton";
     document.getElementById("divButtons").appendChild(crearPenjatPrimitive);
+
+    var crearPokeDex = document.createElement("button");
+    crearPokeDex.onclick =  function() { window.location.href = "Arrays/index.html"; };
+    crearPokeDex.textContent = "PokéDex experimental";
+    crearPokeDex.id = "singleButton";
+    document.getElementById("divButtons").appendChild(crearPokeDex);
 }
 
 function createOldVersionAlert(respuesta) {
-
-    var createOldVersionAlertDiv = document.createElement("div");
-    createOldVersionAlertDiv.id = "oldVersionAlertDiv";
-
-    var createOldVersionAlert = document.createElement("h2");
-    createOldVersionAlert.id = "oldVersionAlert";
-    createOldVersionAlert.innerHTML = "¡Atención! La versión que estás a punto de ejecutar es muy antigua y podría causar problemas de estabilidad en el navegador si sales de ella repentinamente. Asegúrate de cerrar bien el juego por el medio indicado dentro del mismo."
-    
-    
-    var createOldVersionAlertConfirm = document.createElement("button");
-    createOldVersionAlertConfirm.id = "oldVersionAlertConfirm";
-    createOldVersionAlertConfirm.className = "oldAlertButton";
-    createOldVersionAlertConfirm.textContent = "Confirmar";
-    createOldVersionAlertConfirm.onclick = function() { window.location.href = respuesta};
-    
-    
-    var createOldVersionAlertReject = document.createElement("button");
-    createOldVersionAlertReject.id = "oldVersionAlertReject";
-    createOldVersionAlertReject.className = "oldAlertButton";
-    createOldVersionAlertReject.textContent = "Cancelar";
-    createOldVersionAlertReject.onclick = function() { closeOldVersionAlert(); };
-
-    var crearAudio = document.createElement("audio");
-    crearAudio.autoplay = "autoplay";
-    crearAudio.id = "audioBroma";
-    var crearSource = document.createElement("source");
-    crearSource.src = "recursos/equisde.mp3";
-    crearSource.type = "audio/mpeg";
-
-    document.body.appendChild(crearAudio);
-    document.getElementById("audioBroma").appendChild(crearSource);
-    
-
-    document.body.appendChild(createOldVersionAlertDiv);
-    document.getElementById("oldVersionAlertDiv").appendChild(createOldVersionAlert);
-    document.getElementById("oldVersionAlertDiv").appendChild(createOldVersionAlertConfirm);
-    document.getElementById("oldVersionAlertDiv").appendChild(createOldVersionAlertReject);
+    playCautionSound();
+    document.getElementById("caution").style.transform = "scale(1)";
 }
 
-function closeOldVersionAlert() {
-    var deleteOldVersionAlert = document.getElementById("oldVersionAlertDiv");
-    var deleteOldVersionAlertSound = document.getElementById("audioBroma");
-    while (deleteOldVersionAlert.firstChild) {
-        deleteOldVersionAlert.firstChild.remove();
-    }
-    deleteOldVersionAlertSound.remove();
-    deleteOldVersionAlert.remove();
+function closeCautionSplash() {
+    document.getElementById("caution").style.transform = "scale(0)";
+}
+
+function playCautionSound() {
+    document.getElementById("cautionSound").currentTime = 0;
+    document.getElementById("cautionSound").play();
 }
